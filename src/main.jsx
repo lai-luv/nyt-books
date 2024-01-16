b16961139a36d1e4596530d32da0d8c96dd8f96b
lai_luv <l.lavaughn319@gmail.com>
2024-01-16 12:53:19 -0500

    erroe message

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import ErrorBoundary from './ErrorBoundery.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    < ErrorBoundary fallback = "Im sorry to inform you the combination  chosen is invalid , please try again " >
     <App />
+    </ErrorBoundary>
   </React.StrictMode>,
 )
