d3392f0eb40f9da65bbf907b0ede42390c446b6a
lai_luv <l.lavaughn319@gmail.com>
2024-01-16 13:34:44 -0500

    testing errors5

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import ErrorBoundary from './ErrorBoundary.jsx'
+import ErrorBoundary from './ErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
